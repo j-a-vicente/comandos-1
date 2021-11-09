160a281f7d5714e84c97ff5d367954fb033dbec1
José Abelardo Vicente Filho <55700120+maxabelardo@users.noreply.github.com>
2021-11-09 10:25:23 -0300

    Create Manutenção = Base de dados em recovey retory.sql

---
+++ SQL Server - Scripts SQL do dia a dia/Manutenção = Base de dados em recovey retory.sql
@@ -0,0 +1,8 @@
+ALTER DATABASE BDCorporativo SET EMERGENCY;
+GO
+ALTER DATABASE BDCorporativo set single_user
+GO
+DBCC CHECKDB (BDCorporativo, REPAIR_ALLOW_DATA_LOSS) WITH ALL_ERRORMSGS;
+GO
+ALTER DATABASE BDCorporativo set multi_user
+GO
