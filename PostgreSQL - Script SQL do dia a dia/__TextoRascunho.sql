606908690bddedb97c86a726b12196ffe1a83035
José Abelardo Vicente Filho <55700120+maxabelardo@users.noreply.github.com>
2021-12-10 05:56:05 -0300

    Update __TextoRascunho.sql

--- PostgreSQL - Script SQL do dia a dia/__TextoRascunho.sql
+++ PostgreSQL - Script SQL do dia a dia/__TextoRascunho.sql
@@ -24,3 +24,42 @@ SELECT
   triggers.event_manipulation
 FROM 
   information_schema.triggers;
+
+  --Listas as Functions
+select n.nspname as function_schema,
+       p.proname as function_name
+from pg_proc p
+left join pg_namespace n on p.pronamespace = n.oid
+where n.nspname not in ('pg_catalog', 'information_schema')
+order by function_schema,
+         function_name;
+
+
+
+ALTER FUNCTION public._add_overview_constraint(name, name, name, name, name, name, integer) OWNER TO mapasculturaisdev;
+
+"CREATE OR REPLACE FUNCTION public._add_overview_constraint(ovschema name, ovtable name, ovcolumn name, refschema name, reftable name, refcolumn name, factor integer)
+ RETURNS boolean
+ LANGUAGE plpgsql
+ STRICT
+AS $function$
+	DECLARE
+		fqtn text;
+		cn name;
+ (...)"
+
+ 
+/*
+   EXECUTE (
+   SELECT string_agg(format('ALTER FUNCTION %I.%I(%I) OWNER TO mapasculturaisdev ', n.nspname,p.proname,pg_get_function_arguments(p.oid)), '; ')
+    FROM pg_proc p
+     LEFT JOIN pg_namespace n on p.pronamespace = n.oid
+     LEFT JOIN pg_language l on p.prolang = l.oid
+     LEFT JOIN pg_type t on t.oid = p.prorettype 
+    WHERE n.nspname not in ('pg_catalog', 'information_schema')
+      AND t.typname <> 'trigger'
+   );*/
+
+
+ALTER FUNCTION public._postgis_selectivity(regclass, text, geometry, text)
+  OWNER TO mapasculturaisdev;
