5a6fdc025d9e48402acb150495385d2b3d992c74
José Abelardo Vicente Filho <55700120+maxabelardo@users.noreply.github.com>
2022-12-30 12:28:19 -0300

    a

--- SQL Server - Scripts SQL do dia a dia/Analise - Index Sugestão.sql
+++ SQL Server - Scripts SQL do dia a dia/Analise - Index Sugestão.sql
@@ -8,6 +8,16 @@ SELECT ROUND(user_seeks * avg_total_user_cost * ( avg_user_impact * 0.01 ),2) AS
      , migs.user_seeks 
      , ROUND(migs.avg_total_user_cost,2) AS 'avg_total_user_cost'
      , migs.avg_user_impact 
+     ,'CREATE INDEX missing_index_' + 
+        CONVERT (varchar, mig.index_group_handle) + '_' + 
+        CONVERT (varchar, mid.index_handle) + ' ON ' + 
+        mid.statement + ' (' + ISNULL (mid.equality_columns, '') + 
+        CASE
+            WHEN mid.equality_columns IS NOT NULL
+            AND mid.inequality_columns IS NOT NULL THEN ','
+            ELSE ''
+        END + ISNULL (mid.inequality_columns, '') + ')' + 
+        ISNULL (' INCLUDE (' + mid.included_columns + ')', '') AS create_index_statement     
 FROM sys.dm_db_missing_index_group_stats AS migs WITH ( NOLOCK )  
 INNER JOIN sys.dm_db_missing_index_groups AS mig WITH ( NOLOCK ) ON migs.group_handle = mig.index_group_handle  
 INNER JOIN sys.dm_db_missing_index_details AS mid WITH ( NOLOCK ) ON mig.index_handle = mid.index_handle 
