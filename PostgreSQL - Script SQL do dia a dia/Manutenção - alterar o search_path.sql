1b390e557d5ef6f589e46bcdd70dae50df756515
José Abelardo Vicente Filho <55700120+maxabelardo@users.noreply.github.com>
2023-03-22 14:31:45 -0300

    Update Manutenção - alterar o search_path.sql

--- PostgreSQL - Script SQL do dia a dia/Manutenção - alterar o search_path.sql
+++ PostgreSQL - Script SQL do dia a dia/Manutenção - alterar o search_path.sql
@@ -1,1 +1,8 @@
-ALTER DATABASE pje_teste SET search_path TO client, core, jt, criminal, public, acl
+ALTER DATABASE pje_teste SET search_path TO client, core, jt, criminal, public, acl;
+
+
+
+SELECT r.rolname, d.datname, rs.setconfig
+FROM   pg_db_role_setting rs
+LEFT   JOIN pg_roles      r ON r.oid = rs.setrole
+LEFT   JOIN pg_database   d ON d.oid = rs.setdatabase
